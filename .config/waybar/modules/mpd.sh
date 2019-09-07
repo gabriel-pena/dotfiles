1e24cb37fcfb86e9c58ad3ce620933539a09d520
gabriel-pena <gabriel-pena@hotmail.com>
2019-09-07 09:48:55 -0300

    Estilização dar bar do MPD

--- .config/waybar/modules/mpd.sh
+++ .config/waybar/modules/mpd.sh
@@ -2,7 +2,8 @@
 NUM_LINES=$(mpc | awk 'END{print NR}')
 if [ "$NUM_LINES" -eq "3" ]; then
 	MUSIC=$(mpc | awk 'NR == 1')
-	echo "{\"text\":\"${MUSIC}\"}"
+	PERC=$(mpc | awk 'NR == 2' | awk '{print substr($4,2,length($4)-3);}')
+	echo "{\"text\":\"${MUSIC}\",\"class\":[\"active\",\"${PERC}\"]}"
 else
-	echo "{\"text\":\"Pausado\"}"
+	echo "{\"text\":\"Parado\"}"
 fi
