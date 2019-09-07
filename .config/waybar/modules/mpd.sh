c7b9ed6f07de60e67d069b3513ffa138fba392b8
gabriel-pena <gabriel-pena@hotmail.com>
2019-09-07 10:03:42 -0300

    Correções de CSS do MPD

--- .config/waybar/modules/mpd.sh
+++ .config/waybar/modules/mpd.sh
@@ -3,7 +3,7 @@ NUM_LINES=$(mpc | awk 'END{print NR}')
 if [ "$NUM_LINES" -eq "3" ]; then
 	MUSIC=$(mpc | awk 'NR == 1')
 	PERC=$(mpc | awk 'NR == 2' | awk '{print substr($4,2,length($4)-3);}')
-	echo "{\"text\":\"${MUSIC}\",\"class\":[\"active\",\"${PERC}\"]}"
+	echo "{\"text\":\"${MUSIC}\",\"class\":\"${PERC}\"}"
 else
 	echo "{\"text\":\"Parado\"}"
 fi
